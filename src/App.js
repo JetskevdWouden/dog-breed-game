24f1ecb9ecf043b5429d8363086235b6845767f5
JetskevdWouden <zegmaarjet@gmail.com>
2019-06-18 15:05:39 +0200

    minus x

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
       <Provider store={store}>
         <div className="App">
           <header className="App-header">
-x            <h1 className="App-title">Dog Breeds App</h1>
+            <h1 className="App-title">Dog Breeds App</h1>
           </header>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
